9ab0e437992bec5c82e4a42589415fc65d4b0633
sanbondeveloper <sanbondeveloper@gmail.com>
2024-02-16 17:51:19 +0900

    Design: 제품 이미지 디자인 수정

--- app/ui/product-item.tsx
+++ app/ui/product-item.tsx
@@ -6,13 +6,13 @@ export default function ProductItem({ product }: { product: IProduct }) {
   return (
     <li>
       <Link href="#" className="group">
-        <div className="relative h-72 rounded-lg bg-gray-200">
+        <div className="relative h-48 overflow-hidden rounded-lg">
           <Image
             fill
             src={product.image}
             alt={product.title}
-            sizes="(min-width: 640p) 50vw, (min-width: 1024px) 33vw, 25vw"
-            className="object-center group-hover:opacity-75"
+            sizes="(min-width: 1280px) 25vw, (min-width: 1024px) 33vw, 50w"
+            className="object-contain group-hover:opacity-75"
           />
         </div>
         <h3 className="mt-4 text-sm text-gray-700">{product.title}</h3>
