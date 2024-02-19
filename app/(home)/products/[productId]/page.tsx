6758976565cdc88789d82d4033f702a844a49584
sanbondeveloper <sanbondeveloper@gmail.com>
2024-02-19 15:34:26 +0900

    Fix: select 오류 수정

--- app/(home)/products/[productId]/page.tsx
+++ app/(home)/products/[productId]/page.tsx
@@ -19,21 +19,6 @@ export default async function ProductDetailPage({ params }: { params: { productI
             <div className="text-2xl font-bold">{product.price}</div>
           </div>
           <div className="mb-4 text-lg">{product.title}</div>
-          <div className="mb-5">
-            <label htmlFor="countries" className="sr-only mb-2 block text-sm font-medium text-gray-900 dark:text-white">
-              옵션 선택
-            </label>
-            <select
-              id="countries"
-              className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-3.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-            >
-              <option selected>옵션 선택</option>
-              <option value="US">United States</option>
-              <option value="CA">Canada</option>
-              <option value="FR">France</option>
-              <option value="DE">Germany</option>
-            </select>
-          </div>
           <div className="mb-5 flex flex-col">
             <button className="rounded-md bg-[#ef6253] px-3 py-3 font-bold text-white">구매하기</button>
             <button className="border-1 mt-2 rounded-md border-[#ccc] px-3 py-3">관심상품 72</button>
