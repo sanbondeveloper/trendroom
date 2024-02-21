704fb9711775483922d5380f968a10894000fd8b
sanbondeveloper <sanbondeveloper@gmail.com>
2024-02-21 10:27:14 +0900

    Feat: Image 컴포넌트 sizes 속성 지정

--- app/ui/product-item.tsx
+++ app/ui/product-item.tsx
@@ -8,7 +8,13 @@ export default function ProductItem({ product }: { product: IProduct }) {
     <li>
       <Link href={`/products/${product.id}`} className="group">
         <div className="relative h-48 w-48 overflow-hidden rounded-lg">
-          <Image fill src={product.image} alt={product.title} className="object-contain group-hover:opacity-75" />
+          <Image
+            fill
+            sizes="200px"
+            src={product.image}
+            alt={product.title}
+            className="object-contain group-hover:opacity-75"
+          />
         </div>
         <h3 className="mt-4 text-sm text-gray-700">{product.title}</h3>
         <p className="mt-1 text-lg font-medium text-gray-900">{dollarToWon(product.price).toLocaleString()}</p>
