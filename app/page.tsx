64c31eabc77c8b0ae22614d77e6fd54bf8fef03b
sanbondeveloper <sanbondeveloper@gmail.com>
2024-04-09 16:21:33 +0900

    파일 이동

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 import { auth } from '@/auth';
-import { getInterests, getProducts } from '../lib/api';
-import { TProductCategories } from '../lib/definitions';
-import ProductsList from '../ui/products-list';
+import { getInterests, getProducts } from './lib/api';
+import { IProduct, TProductCategories } from './lib/definitions';
+import ProductsList from './ui/products-list';
 
 export default async function Home({ searchParams }: { searchParams?: { tab?: TProductCategories; query?: string } }) {
   const category = searchParams?.tab || 'all';
   const query = searchParams?.query || '';
   const products = await getProducts(category, query);
-  const interests = (await getInterests()) || [];
+  const interests: IProduct[] = [];
   const session = await auth();
 
   // console.log('session', session);
